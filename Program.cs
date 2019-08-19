9244c16bb3121a82de584515457dc5189289bc07
Christa Stephens <christastephens@quickenloans.com>
2019-08-19 15:47:14 -0400

    math for perimeter and area

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace Perimeter
     {
         static void Main(string[] args)
         {
-
+            do
+            {
             Console.WriteLine("Hello Friend! Welcome to the Home Calculator!");
             Console.WriteLine("Please enter your name:");
             string name = Console.ReadLine();
@@ -14,7 +15,22 @@ namespace Perimeter
             Console.WriteLine("Thank you " + name + "!" + "\r\n");
             Console.WriteLine("Let's get to your room, what is the name of your room?");
             string room = Console.ReadLine();
-            Console.WriteLine("Awesome " + name + "!"+" Let's get to measuring your " + room + ".");
+            Console.WriteLine("Awesome " + name + "!"+" Let's get to measuring your " + room + "." + "\r\n");
+
+            Console.WriteLine("Enter the length:");
+            double length = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the width:");
+            double width = double.Parse(Console.ReadLine());
+
+            double pm =  2 *(width + length);
+            double area = length * width;
+            Console.WriteLine("The area of " + room + " is " + area + ".");
+            Console.WriteLine("The perimeter of " + room + " is " + pm + "." + "\r\n");
+
+            Console.WriteLine("Would you like to measure another room " + name + "?" + " Please enter Y/N.");
+            string finish = Console.ReadLine();
+            }
+            while(true);
 
         }
     }
